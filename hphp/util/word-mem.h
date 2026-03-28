e59c7b1ca6c13a39c392ff8f99903375e2dd7f21
Ben Niu <benniu@meta.com>
2026-03-27 21:10:50 -0700

    Use NEON register loads and stores in bcopy32_inline and memcpy16_inline
    Summary: As titled.
    
    Reviewed By: ottoni
    
    Differential Revision: D98159841
    
    fbshipit-source-id: fec408cedc1292d8971f0cd5ff8e62dd643ca0ca

--- hphp/util/word-mem.h
+++ hphp/util/word-mem.h
@@ -111,19 +111,15 @@ inline void bcopy32_inline(void* dst, const void* src, size_t len) {
                        :: "xmm0", "xmm1"
                       );
 #elif defined(__aarch64__)
-  int64_t t3, t4, t5, t6, t7;
+  char __attribute__((vector_size(16))) vt0, vt1;
   __asm__ __volatile__("lsr    %x0, %x0, #5\n"
-                       "sub    %x1, %x1, #16\n"
-                       "sub    %x2, %x2, #16\n"
                        ASM_LOCAL_LABEL("BCP32%=") ":\n"
-                       "ldp    %x3, %x4, [%x1, #16]\n"
-                       "ldp    %x5, %x6, [%x1, #32]!\n"
-                       "stp    %x3, %x4, [%x2, #16]\n"
-                       "stp    %x5, %x6, [%x2, #32]!\n"
+                       "ldp    %q3, %q4, [%x1], #32\n"
+                       "stp    %q3, %q4, [%x2], #32\n"
                        "subs   %x0, %x0, #1\n"
                        "bgt    " ASM_LOCAL_LABEL("BCP32%=") "\n"
                        : "+r"(len), "+r"(src), "+r"(dst),
-                         "=r"(t3), "=r"(t4), "=r"(t5), "=r"(t6), "=r"(t7)
+                         "=&w"(vt0), "=&w"(vt1)
                        :: "cc"
                       );
 #else
@@ -152,28 +148,23 @@ inline void memcpy16_inline(void* dst, const void* src, size_t len) {
                        :: "xmm0", "xmm1"
                       );
 #elif defined(__aarch64__)
-  int64_t t3, t4, t5, t6, s1, d1, d2;
-  __asm__ __volatile__("mov    %x7, %x1\n"
-                       "add    %x1, %x1, %x0\n"
-                       "ldp    %x3, %x4, [%x1, #-16]!\n"
-                       "mov    %x8, %x2\n"
-                       "add    %x2, %x2, %x0\n"
-                       "stp    %x3, %x4, [%x2, #-16]!\n"
+  char __attribute__((vector_size(16))) vt0, vt1;
+  int64_t s1, d1;
+  __asm__ __volatile__("add    %x5, %x1, %x0\n"
+                       "add    %x6, %x2, %x0\n"
+                       "ldr    %q3, [%x5, #-16]\n"
+                       "str    %q3, [%x6, #-16]\n"
                        "lsr    %x0, %x0, #5\n"
                        "cbz    %x0, " ASM_LOCAL_LABEL("END%=") "\n"
-                       "sub    %x7, %x7, #16\n"
-                       "sub    %x8, %x8, #16\n"
                        ASM_LOCAL_LABEL("R32%=") ":\n"
-                       "ldp    %x3, %x4, [%x7, #16]\n"
-                       "ldp    %x5, %x6, [%x7, #32]!\n"
-                       "stp    %x3, %x4, [%x8, #16]\n"
-                       "stp    %x5, %x6, [%x8, #32]!\n"
+                       "ldp    %q3, %q4, [%x1], #32\n"
+                       "stp    %q3, %q4, [%x2], #32\n"
                        "subs   %x0, %x0, #1\n"
                        "bgt    " ASM_LOCAL_LABEL("R32%=") "\n"
                        ASM_LOCAL_LABEL("END%=") ":\n"
                        : "+r"(len), "+r"(src), "+r"(dst),
-                         "=r"(t3), "=r"(t4), "=r"(t5), "=r"(t6),
-                         "=r"(s1), "=r"(d1), "=r"(d2)
+                         "=&w"(vt0), "=&w"(vt1),
+                         "=&r"(s1), "=&r"(d1)
                        :: "cc"
                       );
 #else
